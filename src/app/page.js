313d79f5d3872dea877212324c5e24156596598c
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-06-01 13:46:21 +0530

    initialized the project

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1 className="text-6xl ">this is kamalnath</h1>
+     <h1 className="text-6xl ">home page</h1>
     </main>
   );
 }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='text-3xl bg-indigo-300'>this is nabr </div>
+  )
+}
+
+export default Nav

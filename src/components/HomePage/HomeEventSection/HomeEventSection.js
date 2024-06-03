248286718a68a17a95a21df9c9dbd99cd5dbc823
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-06-03 16:29:53 +0530

    deploy test

--- src/components/HomePage/HomeEventSection/HomeEventSection.js
+++ src/components/HomePage/HomeEventSection/HomeEventSection.js
@@ -9,7 +9,7 @@ export default function Home() {
        
        <h1 className="text-3xl font-bold mb-6">Welcome to the Event  App</h1>
 
-      <div className=" flex  items-center justify-around   ">
+      <div className=" flex  items-center justify-around   lg:w-[90%] w-full mx-auto flex-wrap">
       <Card heading="Orgnize a Event" image={formImage} cardDetail="Here you can orgnized the event" bgColor="bg-gradient-to-l from-cyan-400 to-sky-600 dark:bg-gradient-to-r dark:from-cyan-500 dark:to-sky-700"  navPage="eventform" />
       <Card heading="Orgnize a Event" image={eventList} cardDetail="Here you can orgnized the event" bgColor="bg-gradient-to-r from-emerald-400 to-teal-500  dark:bg-gradient-to-r dark:from-emerald-500 dark:to-teal-600"  navPage="eventlist" />
       <Card heading="Orgnize a Event" image={eventList} cardDetail="Here you can orgnized the event" bgColor="bg-gradient-to-r from-orange-400 to-orange-600 dark:bg-gradient-to-r dark:from-orange-500 dark:to-orange-700" navPage="eventdetail/0"/>
